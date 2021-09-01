ab2e6129397b4c720ae5f4b5bb7f32a6bfe585fe
maxthestranger <thestrangermax@gmail.com>
2021-09-01 13:12:25 +0300

    mongo_express_ejs setup

---
+++ server.js
@@ -0,0 +1,30 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+// routes
+const indexRouter = require('./routes/index');
+
+// view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+// setup mongoose
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected successfuly'));
+
+// routers
+app.use('/', indexRouter);
+
+// port
+app.listen(process.env.PORT || 3000);
